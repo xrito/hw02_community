ffbb3b5e2605373f9468bdab8c4ab559ffbc1953
Дмитрий <xrito@yandex.ru>
2021-09-02 22:12:57 +0400

    Создание фильтра. Приложение core

--- yatube/users/urls.py
+++ yatube/users/urls.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.views import LogoutView
 from django.urls import path
+from . import views
 
 app_name = 'users'
 
@@ -12,4 +13,7 @@ urlpatterns = [
         LogoutView.as_view(template_name='users/logged_out.html'),
         name='logout'
     ),
+    # Полный адрес страницы регистрации - auth/signup/,
+    # но префикс auth/ обрабатывется в головном urls.py
+    path('signup/', views.SignUp.as_view(), name='signup'),
 ]
--- yatube/users/views.py
+++ yatube/users/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+# Функция reverse_lazy позволяет получить URL по параметрам функции path()
+# Берём, тоже пригодится
+from django.urls import reverse_lazy
+# Импортируем CreateView, чтобы создать ему наследника
+from django.views.generic import CreateView
 
-# Create your views here.
+from .forms import CreationForm
+
+
+class SignUp(CreateView):
+    form_class = CreationForm
+    # После успешной регистрации перенаправляем пользователя на главную.
+    success_url = reverse_lazy('posts:index')
+    template_name = 'users/signup.html'
