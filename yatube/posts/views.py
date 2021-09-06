e5c6d0abc6563446ecdc5e32edaeca0a9e909f6f
Дмитрий <xrito@yandex.ru>
2021-09-06 20:42:31 +0400

    Paginator

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,15 +1,28 @@
+from django.core.paginator import Paginator
 from django.shortcuts import get_object_or_404, render
 
 from .models import Group, Post
 
 
 def index(request):
-
-    posts = Post.objects.all()[:10]
+    post_list = Post.objects.all()
+    # Показывать по 10 записей на странице.
+    paginator = Paginator(post_list, 10)
+    # Из URL извлекаем номер запрошенной страницы - это значение параметра page
+    page_number = request.GET.get('page')
+    # Получаем набор записей для страницы с запрошенным номером
+    page_obj = paginator.get_page(page_number)
+    # Отдаем в словаре контекста
     context = {
-        'posts': posts,
+        'page_obj': page_obj,
     }
-    return render(request, 'posts/index.html', context)
+    return render(request, 'posts/index.html', context) 
+
+    # posts = Post.objects.all()[:10]
+    # context = {
+    #     'posts': posts,
+    # }
+    # return render(request, 'posts/index.html', context)
 
 
 def group_posts(request, slug):
